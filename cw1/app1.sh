8b8ca7fc1bb33ce0a72fb7444560710f6e084af4
pspirydo <01187285@pw.edu.pl>
2025-11-20 21:17:59 +0100

    add script for starting client and server

---
+++ cw1/app1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd client/
+docker build -t z37_client .
+
+cd ../server/
+docker build -t z37_server .
+
+docker run -t --name z37_server \
+  --network-alias z37_server \
+  --network z37_network \
+  z37_server:latest &
+
+cd ../client/
+docker run -t --network z37_network z37_client:latest z37_server 8000 15 &
