1dd2cdadf3d1372434180e9e97641298553ee663
Krzysztof Wojtaszko <01178615@pw.edu.pl>
2025-11-27 19:38:20 +0100

    tree structure in server

--- cw2/server/tcp_server.py
+++ cw2/server/tcp_server.py
@@ -13,6 +13,12 @@ else:
 TREE_SIZE = 15
 
 
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
 def recv_exact(conn, n):
     data = b""
     while len(data) < n:
@@ -21,6 +27,29 @@ def recv_exact(conn, n):
             return None
         data += chunk
     return data
+
+def build_tree(values):
+    if values[0] is None:
+        return None
+    root = Node(values[0])
+    queue = [(root, 0)]
+    while queue:
+        node, idx = queue.pop(0)
+        left_i = 2 * idx + 1
+        right_i = 2 * idx + 2
+        if left_i < len(values) and values[left_i] is not None:
+            node.left = Node(values[left_i])
+            queue.append((node.left, left_i))
+        if right_i < len(values) and values[right_i] is not None:
+            node.right = Node(values[right_i])
+            queue.append((node.right, right_i))
+    return root
+
+def inorder(node):
+    if node:
+        inorder(node.left)
+        print(node.val, end=" ")
+        inorder(node.right)
 
 def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -31,17 +60,21 @@ def main():
     conn, addr = server.accept()
     print("Połączono z:", addr)
 
-    tree = [None] * TREE_SIZE
-
-    for i in range(TREE_SIZE):
+    values = [None] * TREE_SIZE
+    for _ in range(TREE_SIZE):
         packet = recv_exact(conn, 8)
         if packet is None:
             break
         node_id, value = struct.unpack("<ii", packet)
-        tree[node_id] = value
+        values[node_id] = value
         print(f"Odebrano: id={node_id}, value={value}")
 
-    print("\nDrzewo BFS:", tree)
+    print("\nRekonstrukcja drzewa...")
+    root = build_tree(values)
+    print("Inorder:", end=" ")
+    inorder(root)
+    print()
+
     conn.close()
     server.close()
 
