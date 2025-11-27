40563f8e7d5441f68bc5e5c74a39be7787b582ea
pspirydo <01187285@pw.edu.pl>
2025-11-27 20:31:08 +0100

    add server and client starting script for lab2

---
+++ cw2/app2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd client/
+docker build -t z37_client .
+
+cd ../server/
+docker build -t z37_server .
+
+docker run -t --name z37_server \
+  --network-alias z37_server \
+  --network z37_network \
+  z37_server:latest &
+
+sleep 5
+cd ../client/
+docker run -t --network z37_network z37_client:latest z37_server 8000 &
