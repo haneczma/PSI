4e7e8a22fc57194a8dc9982fd5fe8dc2cef2b12a
pspirydo <01187285@pw.edu.pl>
2025-11-27 20:30:48 +0100

    correct script for lab1

--- cw1/app1.sh
+++ cw1/app1.sh
@@ -10,5 +10,6 @@ docker run -t --name z37_server \
   --network z37_network \
   z37_server:latest &
 
+sleep 5
 cd ../client/
 docker run -t --network z37_network z37_client:latest z37_server 8000 15 &
