ce55e220e4dcbed078e16b999eb4f8f9c411a5d4
Krzysztof Wojtaszko <01178615@pw.edu.pl>
2025-12-04 10:56:59 +0100

    client update

--- cw3/client/udp_client.py
+++ cw3/client/udp_client.py
@@ -3,24 +3,17 @@ import socket
 import hashlib
 import os
 import struct
-import time
-
-SERVER = "127.0.0.1"
-PORT = 8000
+import sys
 
 PACKET_SIZE = 100
 ACK_TIMEOUT = 0.3
 MAX_RETRIES = 20
-
 FILE_PATH = "data.bin"
-
 
 def generate_random_file(path, size=10000):
     if not os.path.exists(path):
         with open(path, "wb") as f:
             f.write(os.urandom(size))
-        print(f"[CLIENT] Wygenerowano plik {path} ({size} B)")
-
 
 def compute_hash(path):
     h = hashlib.sha256()
@@ -29,62 +22,47 @@ def compute_hash(path):
             h.update(chunk)
     return h.hexdigest()
 
-
 def main():
+    server = sys.argv[1]
+    port = int(sys.argv[2])
+
     generate_random_file(FILE_PATH)
     file_hash = compute_hash(FILE_PATH)
-    print("[CLIENT] Hash klienta:", file_hash)
 
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
         sock.settimeout(ACK_TIMEOUT)
 
         with open(FILE_PATH, "rb") as f:
             seq = 0
-
             while True:
                 data = f.read(PACKET_SIZE)
                 if not data:
                     break
-
                 packet = struct.pack("!I", seq) + data
-
                 retries = 0
                 while True:
-                    print(f"[SEND] seq={seq}, len={len(data)}")
-                    sock.sendto(packet, (SERVER, PORT))
-
+                    sock.sendto(packet, (server, port))
                     try:
                         ack, _ = sock.recvfrom(4)
                         ack_seq = struct.unpack("!I", ack)[0]
-
                         if ack_seq == seq:
-                            print(f"[ACK] seq={seq} OK")
                             break
-                        else:
-                            print(f"[ACK] błędny nr seq={ack_seq}, oczekiwano={seq}")
-
                     except socket.timeout:
                         retries += 1
-                        print(f"[TIMEOUT] powtarzam seq={seq}, próba {retries}")
                         if retries > MAX_RETRIES:
-                            print("[ERROR] zbyt wiele retransmisji, przerwano")
                             return
-
                 seq += 1
-
-        print("[CLIENT] Wszystkie pakiety wysłane. Oczekiwanie na hash z serwera...")
 
         server_hash, _ = sock.recvfrom(1024)
         server_hash = server_hash.decode()
 
-        print("[CLIENT] Hash serwera:", server_hash)
-        print("[CLIENT] Hash klienta :", file_hash)
+        print(file_hash)
+        print(server_hash)
 
         if server_hash == file_hash:
-            print("\n[SUKCES] Hashe są identyczne. Transmisja prawidłowa")
+            print("OK")
         else:
-            print("\n[BŁĄD] Hash różny — plik uszkodzony podczas transmisji")
-
+            print("BAD")
 
 if __name__ == "__main__":
     main()
